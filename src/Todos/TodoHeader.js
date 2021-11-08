247fe96c6adbea9312d24b5e3d9aa818e5e56cbc
Dovile Bartkeviciute <d.bartkeviciute96@gmail.com>
2021-11-08 16:44:42 +0200

    Refactor TodoHeader to use styled components

--- src/Todos/TodoHeader.js
+++ src/Todos/TodoHeader.js
@@ -1,5 +1,49 @@
-import css from '../styles/TodoHeader.module.css';
+import styled from "styled-components";
+import Icon from "./Icon";
 import TimeDisplay from './Time';
+
+const Header = styled.header`
+    position: relative;
+`;
+
+const HeaderIconContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: absolute;
+    right: 20px;
+    top: 20px;
+
+    i {
+        color: #fff;
+        font-size: 2rem;
+        :nth-child(2) {
+            margin-top: 0.5rem;
+        }
+    }
+`;
+
+const HeaderInfo = styled.p`
+    position: absolute;
+    width: calc(100% - 40px);
+    bottom: 10px;
+    color: #fff;
+    left: 20px;
+    background-color: rgba(0, 0, 0, 0.507);
+    display: flex;
+    justify-content: space-between;
+    padding: 0.5rem;
+`;
+
+const HeaderImg = styled.img`
+    max-height: 200px;
+    border-top-left-radius: 4px;
+    border-top-right-radius: 4px;
+    width: 100%;
+    height: auto;
+    object-fit: cover;
+    object-position: bottom;
+`;
 
 function TodoHeader(props) {
     const onReset = () => {
@@ -9,24 +53,23 @@ function TodoHeader(props) {
     let completedTasks = props.todos.filter(tObj => tObj.isDone);
 
     return (
-        <header>
-        <div className={css.clear}>
-            <i id='reset' className='fa fa-refresh' onClick={onReset}></i>
-            {props.isLoading ? '' : <i className='fa fa-photo' onClick={props.onNewHeaderImage}></i>}
-            
-        </div>
-        <p id='date' className={css.date}>
-            {props.isLoading ? <span>Loading...</span> : <TimeDisplay />}
-            <span>
-                Tasks completed: {completedTasks.length}/{props.todos.length}
-            </span>
-        </p>
-        <img
-            className={`${css['img-fluid']} ${css['cover-img']}`}
-            src={props.headerImage}
-            alt='Cover'
-        />
-        </header>
+        <Header>
+            <HeaderIconContainer>
+                <Icon iconName='refresh' clickFunc={onReset}></Icon>
+                {props.isLoading ? '' : <Icon iconName='photo' clickFunc={props.onNewHeaderImage}></Icon>}
+                
+            </HeaderIconContainer>
+            <HeaderInfo>
+                {props.isLoading ? <span>Loading...</span> : <TimeDisplay />}
+                <span>
+                    Tasks completed: {completedTasks.length}/{props.todos.length}
+                </span>
+            </HeaderInfo>
+            <HeaderImg
+                src={props.headerImage}
+                alt='Cover'
+            />
+        </Header>
     );
 }
 
